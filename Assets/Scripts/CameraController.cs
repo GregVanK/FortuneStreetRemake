c82035761f70d3b58a769f4e8cf09854ced176bb
DESKTOP-PQRCVO2\Googis <gvankampen@upei.ca>
2020-10-27 22:05:35 -0300

    added camera follow + demo

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    void Start()
+    {
+       
+    }
+
+    
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z);
+        
+    }
+}
---
+++ Assets/Scripts/CharacterMoveExample.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoveExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(this.transform.position.x + 0.001f, this.transform.position.y, this.transform.position.z);
+    }
+}
