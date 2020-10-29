bf4c970ab8e7328a7779840d833f2a12a286c92c
DESKTOP-PQRCVO2\Googis <gvankampen@upei.ca>
2020-10-28 22:35:44 -0300

    added dice stuff

---
+++ Assets/Scripts/DiceRoll.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceRoll : MonoBehaviour
+{
+
+    public float faceTimer;
+    public Sprite[] diceFaces;
+    public SpriteRenderer spriteRenderer;
+    private int currentFace;
+    private float cycleSpeed;
+    private bool isStopped;
+    void Start()
+    {
+        currentFace = 1;
+        faceTimer = Time.time;
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = diceFaces[currentFace-1];
+        cycleSpeed = 0.08f;
+        isStopped = false;
+}
+
+    //update dice every cycle
+    void Update()
+    {
+        //Stop if dice has been slowed down
+        if (cycleSpeed <= 2f)
+        {
+            //face timer increments until it's time to change face.
+            if (faceTimer > cycleSpeed)
+            {
+                updateDice();
+                if (isStopped)
+                    cycleSpeed += 0.5f;
+            }
+            else
+            {
+                faceTimer += Time.deltaTime;
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+                isStopped = true;
+        }
+        
+            
+    }
+
+    //Change dice face to next face and update sprite
+    void updateDice()
+    {
+        faceTimer = 0f;
+        if (currentFace >= 6)
+            currentFace = 1;
+        else
+            currentFace++;
+        spriteRenderer.sprite = diceFaces[currentFace - 1];
+    }
+}
