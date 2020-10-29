3d1843daf6ad161eeaac9e2582a516f8531a4060
MoonaChannel <55731548+MoonaChannel@users.noreply.github.com>
2020-10-28 19:53:58 -0500

    made the camera follow the player smoothly + placeholder movement code for player

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform player;
+    public Transform target;
+    
+    public float smoothTime = 0.3f;
     public Vector3 offset;
-    void Start()
-    {
-       
-    }
+    private Vector3 velocity = Vector3.zero;
 
     
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z);
-        
+        Vector3 desiredPosition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothTime);
     }
 }
