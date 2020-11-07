f8f4fc73c95c06a4ab6f6c5a0e16727725ad0ca4
DESKTOP-PQRCVO2\Googis <gvankampen@upei.ca>
2020-11-07 13:59:51 -0400

    +added scene with client connection menu +implimented network & UI manager

---
+++ Assets/Scripts/NetworkClient.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lidgren.Network;
+using UnityEngine.UI;
+using System;
+
+public class NetworkClient : MonoBehaviour
+{
+
+    NetPeerConfiguration config;
+    public static NetworkClient instance;
+    public NetClient client;
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.Log("Network Client already exists.");
+            Destroy(this);
+        }
+    }
+    public void Start()
+    {
+        config = new NetPeerConfiguration("LuckyRoad");
+        client = new NetClient(config);
+        client.Start();
+    }
+
+    //todo add username functionality
+    public void connectToServer(string ipData, int portData)
+    {
+        client.Connect(host: ipData, port: portData);
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+    public GameObject connectElements;
+    public InputField usernameInput;
+    public InputField ipAddressInput;
+    public InputField portInput;
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.Log("UI Manager already exists.");
+            Destroy(this);
+        }
+    }
+
+    public void beginConnection()
+    {
+        connectElements.SetActive(false);
+        usernameInput.interactable = false;
+        ipAddressInput.interactable = false;
+        portInput.interactable = false;
+
+        //TODO: add text formatting error handling
+        NetworkClient.instance.connectToServer(ipAddressInput.text,Int32.Parse(portInput.text));
+    }
+}
